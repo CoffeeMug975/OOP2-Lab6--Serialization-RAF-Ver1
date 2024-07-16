faa949fb9620591ba73fb40a9c5e2accc04a25b0
DSAsefa <danielasefa975@gmail.com>
2024-07-16 00:23:56 -0600

    Finished questions 1-4
    - Created Event Class
    - Instantiate Event Object
    - Serialize Object to JSON File:
    - Deserialize Object from JSON File

---
+++ Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace OOP2_Lab6__Serialization_RAF
+{
+    [Serializable]
+    public class Event
+    {
+        // Set the properties for the class
+
+        public int EventNumber { get; set; }
+        public string Location { get; set; }
+
+        //public Event(int eventNumber, string location)
+        //{
+        //    EventNumber = eventNumber;
+        //    Location = location;
+        //}
+        public override string ToString()
+        {
+            string info = ($"Event Number: {EventNumber}\nLocation: {Location}\n");
+            return info;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿namespace OOP2_Lab6__Serialization_RAF
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text.Json;
+using System.Collections.Generic;
+namespace OOP2_Lab6__Serialization_RAF
 {
     internal class Program
     {
@@ -7,17 +12,41 @@
             Console.WriteLine("Lab 6 : Using Serialization and Random Access Files\n" +
                 "CPRG-211-F\n" + "Daniel Asefa\n\n");
             
+
+            // Question 1 & 2
             Console.WriteLine("Press Enter to initalize the creation of the Event object:");
             Console.ReadLine();
 
+            Event newEvent = new Event() {EventNumber = 1, Location = "Calgary"};
+
+
+            // Question 3
             Console.WriteLine("Press Enter to serialize the object to a JSON file named: \"event.json\"");
             Console.ReadLine();
 
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(newEvent, options);
+            File.WriteAllText("event.json", jsonString);
+
+
+            // Question 4
             Console.WriteLine("Press Enter to deserialize JSON file and display object values to console");
             Console.ReadLine();
 
+            string filePath = "event.json";
+            
+            string newJsonString = File.ReadAllText(filePath);
+            Event e = JsonSerializer.Deserialize<Event>(jsonString);
+
+            Console.WriteLine(e.ToString());
+
+
+            // Question 5
             Console.WriteLine("Press Enter to write \"Hackathon\" to JSON file & display first, middle and last characters");
             Console.ReadLine();
+
+            
+
         }
     }
 }
